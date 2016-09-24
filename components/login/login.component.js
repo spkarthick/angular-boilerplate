65fa18eeeb0a733ca820ef50f1914988473eea30
spkarthick <spkarthick91@gmail.com>
2016-09-24 14:02:46 +0530

    Please enter the commit message for your changes. Lines starting

--- components/login/login.component.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	var module = angular.module("login");
-	
-	module.component("login", {
-		templateUrl: "/components/login/login.html",
-		controller: "loginController",
-		controllerAs: "vm"
-	});
-})();
--- components/login/login.service.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	
-	var module = angular.module("login");
-	
-	module.factory("loginService", ["$http", function($http) {
-		 return {};
-	}]);
-	
-})();
